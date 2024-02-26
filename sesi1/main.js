516db04712798852e5099b2ef711a359b6118c6e
wahyuWs <wahyusantozo36@gmail.com>
2024-02-26 12:31:36 +0700

    initial commit

---
+++ sesi1/main.js
@@ -0,0 +1,68 @@
+// menampilkan alert
+// alert('semoga harimu indah')
+
+// menampilkan pesan di console
+// console.log('Semoga harimu menyenangkan')
+
+// membuat inputan prompt
+// prompt('Nama hewan kesukaan ?')
+
+// inisialisasi variable
+// var day = 'Rabu'
+// const color = 'Red' //final variabel
+// let vehicle = 'Car' //dinamis variabel
+// console.log('Hari fovorite mu: ' + day)
+
+// input prompt tampil alert
+// var age = prompt('Berapa usia mu ?')
+// alert('Usia mu saat ini: ' + age)
+
+// tipe data
+let name = 'Wahyu Santoso' //string
+let age = 24 //integer or number
+let height = 164.5 //float
+let status = null //null value
+let saldoPertama = 50000
+let saldoKedua = 100000
+
+
+//if statement
+// if (status == null)
+// { status = 'Belum Menikah' } 
+// else { status = 'Sudah Menikah' }
+
+// switch statement
+// switch (status) {
+//     case null:
+//         status = 'Belum Menikah'
+//         break;
+//     case 1:
+//         status = 'Sudah Menikah'
+//         break;
+//     default:
+//         status = 'Masih mencari'
+//         break;
+// }
+
+// alert(`Nama saya: ${name} umur saya: ${age} dengan tinggi badan ${height} dan status saya ${status}`)
+
+// operator
+// alert(`Saldo pertama saya Rp.${saldoPertama} saldo kedua Rp.${saldoKedua}. Total saldo saya Rp.${saldoPertama + saldoKedua}`)
+
+// array
+// let day = [
+//     'senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu'
+// ]
+// //add
+// day.push('minggu', 'senin')
+// day.shift() //hapus index awal
+// day.pop() //hapus index akhir
+// alert(`hari dalam array: ${day}`)
+
+// for loop
+// let value = []
+// alert(`nilai value sebelum di loop: `)
+// for (let index = 0; index < 8; index++) {
+//     value.push(index)
+// }
+// alert(`nilai value setelah di loop: ${value}`)
---
+++ sesi1/task.js
@@ -0,0 +1,7 @@
+/*
+1. prompt untuk mengetahui saldo akhir dari apa yang di inputkan oleh user.
+2. menentukan hari dari tanggal saat ini di pc kalian.
+contoh:
+let hari = new Date().getDay()
+//tampilkan hari nya bukan number
+**/
